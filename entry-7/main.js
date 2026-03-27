f2f76866bcbc4ee2ed286f8715d3c9f25138e156
hoffs756-jpg <hoffs756@newschool.edu>
2026-03-27 15:34:26 +0000

    esfjfsj

---
+++ entry-7/main.js
@@ -0,0 +1,14 @@
+const phrases = [
+  "green",
+  "cat",
+  "sits happily"
+
+];
+
+function showPhrases(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+showPhrases(phrases);
